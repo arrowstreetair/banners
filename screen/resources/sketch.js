480c515cf68e84f666c9b89ba64a4ad448a3e654
Tyler Shannon <Shannon@SEANs-MacBook-Pro-2.local>
2019-06-11 12:55:57 -0400

    create canvas

---
+++ screen/resources/sketch.js
@@ -0,0 +1,15 @@
+function preload() {
+  //img = loadImage('./image_background.jpg');
+  //fontBanner = loadFont('./MetaOT-Light.otf');
+}
+
+function setup() {
+  var canvas = createCanvas(windowWidth,windowWidth/4);
+  canvas.parent("main");
+}
+
+function draw() {
+
+  background(255);
+
+}
