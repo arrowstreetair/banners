407e918586685ab679736c6a5d5de11e63d35889
Tyler Shannon <Shannon@SEANs-MacBook-Pro-2.local>
2019-06-17 15:21:37 -0400

    new banner

--- screen/resources/sketch.js
+++ screen/resources/sketch.js
@@ -5,6 +5,11 @@ let amplitude = 75.0;
 let xspacing = 16;
 let period = 500.0;
 let theta = 0.0;
+let starty = 1;
+let xpos, ypos;
+let yspeed = 1;
+let xdirection = 1;
+let ydirection = 1;
 
 
 function preload() {
@@ -19,6 +24,9 @@ function setup() {
   w = windowWidth;
   dx = (TWO_PI / period) * xspacing;
   yvalues = new Array(floor(w/xspacing));
+
+  xpos = 0;
+  ypos = 0;
 }
 
 function draw() {
@@ -32,9 +40,23 @@ function draw() {
   if (y>windowWidth/4){
     y = windowWidth/4;
   }
-  for (var i=0;i<windowWidth/4-100;i+=10){
-    bezier(0,i,x,y,x,y,windowWidth,i);
+  for (var i=starty;i<windowWidth/4-100;i+=10){
+    //bezier(0,i,x,y,x,-y,windowWidth,i);
   }
+
+  ypos = ypos + yspeed * ydirection;
+  //if (ypos > height - 20 || ypos < 0) {
+    //ydirection *= -1;
+  //}
+  //fill(0);
+  //ellipse(xpos, ypos, 20,20);
+  for (var i=0;i<200;i+=10){
+    noFill();
+    bezier(0,ypos+i,x,y,x,-y,windowWidth,ypos+i)
+    createEllipse(ypos+i);
+  }
+
+
 }
 
 function windowResized() {
@@ -43,4 +65,13 @@ function windowResized() {
   w = windowWidth;
   dx = (TWO_PI / period) * xspacing;
   yvalues = new Array(floor(w/xspacing));
+}
+
+function createEllipse(ypos){
+  ypos = ypos + yspeed * ydirection;
+  if (ypos > height - 20 || ypos < 0) {
+    ydirection *= -1;
+  }
+  fill(0);
+  //ellipse(xpos, ypos, 20,20);
 }
