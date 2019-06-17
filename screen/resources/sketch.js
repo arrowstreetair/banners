9aa74221229e32d4b5aef1b2c53a6166d9f9f613
Tyler Shannon <Shannon@SEANs-MacBook-Pro-2.local>
2019-06-17 14:35:40 -0400

    new banner

--- screen/resources/sketch.js
+++ screen/resources/sketch.js
@@ -26,18 +26,15 @@ function draw() {
   background(255);
   noFill();
   strokeWeight(.1);
+  stroke('#3399ff');
   let x = mouseX;
   let y = mouseY;
   if (y>windowWidth/4){
     y = windowWidth/4;
   }
-  for (var i=0;i<windowWidth/4-60;i+=10){
+  for (var i=0;i<windowWidth/4-100;i+=10){
     bezier(0,i,x,y,x,y,windowWidth,i);
   }
-  //bezier(0,100,mouseX,mouseY,mouseX,mouseY,windowWidth,200);
-  //bezier(0,110,mouseX,mouseY,mouseX,mouseY,windowWidth,190);
-  //bezier(0,120,mouseX,mouseY,mouseX,mouseY,windowWidth,180);
-
 }
 
 function windowResized() {
