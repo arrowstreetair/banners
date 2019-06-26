0c6613d9662805887abfc07a6bae70b8a1d56e84
Tyler Shannon <Shannon@SEANs-MacBook-Pro-2.local>
2019-06-26 12:04:17 -0400

    update

--- screen/resources/sketch0.js
+++ screen/resources/sketch0.js
@@ -7,7 +7,7 @@ let starty = 1;
 let xpos_left, ypos_left, xpos_right, ypos_right;
 let xpos_cp1, ypos_cp1;
 let xpos_cp2, ypos_cp2;
-let yspeed = .5;
+let yspeed = .25;
 let left_direction = 1;
 let right_direction;
 let cp1_directions = [-1,1];
@@ -15,6 +15,7 @@ let cp1_direction;
 let cp2_direction;
 var canvas;
 let step;
+let curve_height;
 
 
 function preload() {
@@ -23,15 +24,16 @@ function preload() {
 }
 
 function setup() {
-  canvas = createCanvas(windowWidth,windowWidth/4);
+  canvas = createCanvas(windowWidth,windowWidth/3);
   canvas.parent("main");
   w = canvas.width;
   h = canvas.height;
+  curve_height = 300;
 
   xpos_left = 0;
   ypos_left = 0;
   xpos_right = w;
-  ypos_right = h;
+  ypos_right = h-curve_height;
   xpos_cp1 = random(w/5,w/5*2);
   ypos_cp1 = random(10,h-10);
   xpos_cp2 = random(w/5*3,w/5*4);
@@ -59,7 +61,7 @@ function draw() {
 
   step = 10;
 
-  for (var i=-h; i<h;i+=step){
+  for (var i=-h; i<curve_height;i+=step){
     //DEFINE LEFT anchor
     //define left anchor movement
     fill(0);
@@ -80,15 +82,22 @@ function draw() {
     fill(0);
     //ellipse(xpos_cp2, ypos_cp2,10,10);
 
-    noFill()
-
-    stroke(0);
+    noFill();
+    //fill(61,158,255,3);
+    stroke(200);
     bezier(xpos_left,ypos_left+i,xpos_cp1,ypos_cp1,xpos_cp2,ypos_cp2,xpos_right,ypos_right+i);
+    for (var t = 0;t<1;t+=.025){
+      let x = bezierPoint(xpos_left,xpos_cp1,xpos_cp2,xpos_right,t);
+      let y = bezierPoint(ypos_left+i,ypos_cp1,ypos_cp2,ypos_right+i,t);
+      fill(250);
+      //ellipse(x,y,2,2);
+    }
+
   }
 
 
   //CHANGE DIRECTION AT EDGE
-  if (ypos_left > h || ypos_left < 0){
+  if (ypos_left > h-curve_height || ypos_left < 0){
     left_direction *= -1;
   }
   if (ypos_cp1 > h || ypos_cp1 < 0){
@@ -103,9 +112,12 @@ function draw() {
 }
 
 function windowResized() {
-  canvas = resizeCanvas(windowWidth, windowWidth/4);
+  canvas = createCanvas(windowWidth,windowWidth/3);
   w = canvas.width;
   h = canvas.height;
+
+  xpos_right = w;
+  ypos_right = h-curve_height;
 }
 
 function changeDirection(ypos){
@@ -118,12 +130,11 @@ function changeDirection(ypos){
   //ellipse(xpos, ypos, 20,20);
 }
 
-function dothis() {
-  xpos_cp1 = mouseX;
-  ypos_cp1 = mouseY;
-}
-
 function mouseMoved(){
-  xpos_cp1 = mouseX;
+  xpos_cp1 = mouseX+500;
+  xpos_cp2 = w - xpos_cp1;
   ypos_cp1 = mouseY;
+  //xpos_right = w - mouseX;
+  //xpos_left = w - xpos_right;
+  bezierDetail(10);
 }
